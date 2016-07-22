9e788199887ea41426d0e6b671852474454cf6c6
WillispYoung <willispyoung@gmail.com>
2016-07-22 13:11:01 +0800

    rough idea

--- GreatLibrary/GreatLibrary/urls.py
+++ GreatLibrary/GreatLibrary/urls.py
@@ -22,4 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', views.home, name="home"),
     # add more urls here
+    url(r'^login/', login, name="login"),
+    url(r'^logout/', logout, name="logout"),
+    url(r'^register/', register, name="register"),
 ]
